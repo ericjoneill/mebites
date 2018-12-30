88e477e3c1cfa62717c3b0d6b1ffb2032a33eca5
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2018-12-29 22:10:32 -0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 
 import x from './test'
+const s = 23;
 
-console.log(`I imported ${x} from another module caled test.js.`)
+console.log(`I imported ${x} from another module caled test.js. Variable s is ${s}`)
