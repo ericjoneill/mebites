e33d9eafaa00de9a767b4e9af6f184f71299bf99
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2018-12-31 16:24:58 -0500

    Add files via upload

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,2 +1,23 @@
-export default 'I  am an exported string.'
+import axios from 'axios'
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+        async getResults(){
+            const proxy = 'https://cors-anywhere.herokuapp.com/'
+            const key = 'b48e381abf5534796a236798cecdcbc6'
+            try {
+                const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+                this.result = res.data.recipes;
+                // console.log(this.result)
+            } catch (error){
+                alert(error);
+            }
+
+// https://www.food2fork.com/api/search
+// b48e381abf5534796a236798cecdcbc6
+    }
+}
 
