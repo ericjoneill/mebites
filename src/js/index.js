10b95363e42de238010c4c41b7e50fae20dae3bc
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2018-12-31 16:06:56 -0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
-import str from './models/Search'
-// import * from './views/searchView'
+import axios from 'axios'
 
-import { add as a, multiply as m, ID } from './views/searchView'
+async function getResults(query){
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = 'b48e381abf5534796a236798cecdcbc6'
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes;
+        console.log(recipes)
+    } catch (error){
+        alert(error);
+    }
+}
+getResults('pizza')
 
-console.log(`Using imported functions! ${a(2,3)} is added and ${m(4,4)} is multiplied. my ID is ${ID}, ${str}`)
+// https://www.food2fork.com/api/search
+// b48e381abf5534796a236798cecdcbc6
