1ce7fedcc47a76e00ff39bf8d61b741de847ee62
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2018-12-31 16:25:30 -0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,32 @@
-import axios from 'axios'
+import Search from './models/Search'
 
-async function getResults(query){
-    const proxy = 'https://cors-anywhere.herokuapp.com/'
-    const key = 'b48e381abf5534796a236798cecdcbc6'
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`)
-        const recipes = res.data.recipes;
-        console.log(recipes)
-    } catch (error){
-        alert(error);
-    }
-}
-getResults('pizza')
+const search = new Search('pizza')
+console.log(search)
+search.getResults()
 
-// https://www.food2fork.com/api/search
-// b48e381abf5534796a236798cecdcbc6
+
+
+
+
+
+
+
+
+
+// import axios from 'axios'
+
+// async function getResults(query){
+//     const proxy = 'https://cors-anywhere.herokuapp.com/'
+//     const key = 'b48e381abf5534796a236798cecdcbc6'
+//     try {
+//         const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+//         const recipes = res.data.recipes;
+//         console.log(recipes)
+//     } catch (error){
+//         alert(error);
+//     }
+// }
+// getResults('pizza')
+
+// // https://www.food2fork.com/api/search
+// // b48e381abf5534796a236798cecdcbc6
