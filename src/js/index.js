01c48d784810d985f5df685ba2846a72f4261cac
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-01 22:29:31 -0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ const state = {}
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput(); // TODO
-
+    console.log(query)
     if (query){
         // 2) New search object and add to state
         state.search = new Search(query)
