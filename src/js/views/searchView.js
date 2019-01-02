1c96776dabcaea83b2fc078d8829d53085e6967f
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-01 21:42:13 -0500

    Add files via upload

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,12 @@
-export const add = (a, b) => a+b;
-export const multiply = (a, b) => a*b;
-export const ID = 23;
+import { elements } from './base'
+
+export const getInput = () => elements.searchInput.value;
+
+
+
+
+
+// export const add = (a, b) => a+b;
+// export const multiply = (a, b) => a*b;
+// export const ID = 23;
+
