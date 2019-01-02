8309eb64b67efc97e088ea8a18ed127acbe175a9
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-01 22:29:11 -0500

    Add files via upload

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,25 @@ import { elements } from './base'
 
 export const getInput = () => elements.searchInput.value;
 
+const renderRecipe = recipe => {
+    const markup = `
+    <li>
+    <a class="results__link results__link--active" href="${recipe.recipe_id}">
+        <figure class="results__fig">
+            <img src="img/test-1.jpg" alt="Test">
+        </figure>
+        <div class="results__data">
+            <h4 class="results__name">Pasta with Tomato ...</h4>
+            <p class="results__author">The Pioneer Woman</p>
+        </div>
+    </a>
+</li>
+`
+}
+
+export const renderResults = recipes => {
+    recipes.forEach(renderRecipe)
+}
 
 
 
