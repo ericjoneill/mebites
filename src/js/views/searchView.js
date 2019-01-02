27ad4654e694d03951b896b667c595d625415586
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-01 23:11:55 -0500

    Add files via upload

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,24 +2,31 @@ import { elements } from './base'
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => { elements.searchInput.value = '';}
+
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+}
+
 const renderRecipe = recipe => {
     const markup = `
     <li>
-    <a class="results__link results__link--active" href="${recipe.recipe_id}">
+    <a class="results__link" href="#${recipe.recipe_id}">
         <figure class="results__fig">
-            <img src="img/test-1.jpg" alt="Test">
+            <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">Pasta with Tomato ...</h4>
-            <p class="results__author">The Pioneer Woman</p>
+            <h4 class="results__name">${recipe.title}</h4>
+            <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
 </li>
 `
+elements.searchResList.insertAdjacentHTML('beforeend', markup)
 }
 
 export const renderResults = recipes => {
-    recipes.forEach(renderRecipe)
+    recipes.forEach(renderRecipe);
 }
 
 
