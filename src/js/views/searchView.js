64400899956052b7c84e2406e3b41d0862429be7
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-02 21:08:15 -0500

    Add files via upload

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -7,18 +7,39 @@ export const clearInput = () => { elements.searchInput.value = '';}
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 }
+/**
+ * Pasta with tomato and spinach
+ * acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+ *acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta','with']
+ *acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta','with','tomato']
+ */
+export const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
 
 const renderRecipe = recipe => {
     const markup = `
     <li>
     <a class="results__link" href="#${recipe.recipe_id}">
-        <figure class="results__fig">
-            <img src="${recipe.image_url}" alt="${recipe.title}">
-        </figure>
-        <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
-            <p class="results__author">${recipe.publisher}</p>
-        </div>
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
     </a>
 </li>
 `
