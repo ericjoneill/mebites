c6de3db052762906910e55830b819c8ad8b5ee8b
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-04 18:23:42 -0500

    Add files via upload

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { key, proxy } from '../config'
+// 656bef8b10c143a7681b10cbdc689bbd
+// b48e381abf5534796a236798cecdcbc6
 
 export default class Search {
     constructor(query) {
@@ -6,8 +9,6 @@ export default class Search {
     }
 
         async getResults(){
-            const proxy = 'https://cors-anywhere.herokuapp.com/'
-            const key = 'b48e381abf5534796a236798cecdcbc6'
             try {
                 const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
                 this.result = res.data.recipes;
