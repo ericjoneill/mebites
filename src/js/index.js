87eb7921de393a3673bac62610a3da07ddf5d9aa
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-13 12:17:47 -0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -152,8 +152,6 @@ const wel = window.addEventListener;
  * LIKE CONTROLLER
  */
 // TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes())
 
 const controlLike = () => {
      if (!state.likes) state.likes = new Likes()
@@ -185,6 +183,17 @@ const controlLike = () => {
      }
      likesView.toggleLikeMenu(state.likes.getNumLikes())
  }
+
+ // Restore liked recipes on page load
+ window.addEventListener('load', () => {
+    state.likes = new Likes();
+    // Restore likes
+    state.likes.readStorage();
+    //Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes())
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like))
+ })
 
 
 // Handling recipe button clicks
