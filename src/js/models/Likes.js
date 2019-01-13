bedc820b067b8e405dd0939a67f90c037f08d2bd
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-13 12:18:09 -0500

    Add files via upload

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -11,11 +11,19 @@ export default class Likes {
             img
         }
         this.likes.push(like)
+
+        // Persist data in localStorage
+        this.persistData();
+
+
         return like;
     }
     deleteLike(id){
         const index = this.likes.findIndex(el => el.id === id)
         this.likes.splice(index, 1)
+
+        // Persist data in localStorage
+        this.persistData()
     }
 
     isLiked(id){
@@ -24,5 +32,16 @@ export default class Likes {
 
     getNumLikes(){
         return this.likes.length
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes))
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes'))
+        // Restoring likes from the localStorage
+        if (storage) this.likes = storage;
+
     }
 }
